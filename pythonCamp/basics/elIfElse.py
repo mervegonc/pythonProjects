40ca4c0ea46a48c82ff645e227ef30bec2256171
merve-goncu <merve.gnc.946@gmail.com>
2025-09-11 12:27:25 +0300

    positive/negatie/zero

---
+++ pythonCamp/basics/elIfElse.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+lights = ["red", "yellow", "green"]
+currentLight = lights[1]
+print(currentLight)
+
+if currentLight == "red":
+    print("stop")
+    
+elif currentLight == "yellow":
+    print("get ready")    
+
+elif currentLight == "green":
+    print("go")
---
+++ pythonCamp/workshops/changingthePositionsOfNumbers.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("Positive")
+elif number == 0:
+    print("Zero")
+elif number <<0:
+    print("Negative")    
