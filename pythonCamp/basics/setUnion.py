b4e1b96555b21e1d2d8e99af0a8be2818e9616c2
merve-goncu <merve.gnc.946@gmail.com>
2025-09-11 11:18:34 +0300

    some set futures

--- pythonCamp/basics/setUnion.py
+++ pythonCamp/basics/setUnion.py
@@ -3,8 +3,18 @@
 setA = {1,2,3,4,5}
 setB = {1,3,4,6,7,8}
 
+#birleştirir aynıolanları yazmadan
 print(setA | setB)
 print(setA.union(setB))
 
+#aynı olanları yazar
 print(setA & setB)
 print(setA.intersection(setB))
+
+#a da olup b de olmayanı yazar
+print(setA - setB)
+print(setB.difference(setA))
+
+
+print(setA ^ setB)
+print(setA.symmetric_difference(setB))
