96c55a1de2ac56795ce3dc82032c2297759e6edb
merve-goncu <merve.gnc.946@gmail.com>
2025-09-11 11:46:55 +0300

    dictionary futures

---
+++ pythonCamp/basics/dictionary.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+dictionary = {
+    "abandon" : "terk etmek",
+"acquire" : "edinmek",
+"ambiguous" : "belirsiz, muğlak",
+"coherent" : "tutarlı",
+"compile" : "derlemek",
+"dilemma" : "ikilem",
+"enhance" : "geliştirmek, iyileştirmek",
+"feasible" : "yapılabilir, uygulanabilir",
+"hierarchy" : "hiyerarşi",
+"integrity" : "dürüstlük, doğruluk",
+"justify" : "haklı çıkarmak",
+"lucrative" : "kârlı",
+"perceive" : "algılamak",
+"reluctant" : "isteksiz, gönülsüz",
+"substantial" : "önemli, esaslı"
+
+    }
+print(dictionary.keys())
--- pythonCamp/basics/setUnion.py
+++ pythonCamp/basics/setUnion.py
@@ -7,14 +7,15 @@ setB = {1,3,4,6,7,8}
 print(setA | setB)
 print(setA.union(setB))
 
-#aynı olanları yazar
+#it writes the same ones
 print(setA & setB)
 print(setA.intersection(setB))
 
-#a da olup b de olmayanı yazar
+#it writes the ones that are in a but not in b in order.
 print(setA - setB)
 print(setB.difference(setA))
 
 
+#Here, it writes the items that are in a but not in b, and the items that are in b but not in a.
 print(setA ^ setB)
 print(setA.symmetric_difference(setB))
