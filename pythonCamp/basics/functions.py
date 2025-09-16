2b73818362126328518498df4d531d865fcec3e2
merve-goncu <merve.gnc.946@gmail.com>
2025-09-16 15:14:19 +0300

    Functions parameters

--- pythonCamp/basics/functions.py
+++ pythonCamp/basics/functions.py
@@ -3,4 +3,17 @@
 city = "Ankara"
 
 print(city.upper())
-print(city.endswith("a"))
+print(city.endswith("a"))#it turns false or true
+
+def sayHi(name = "dearist"):
+    print("Hello " + name)
+ 
+    
+def sayHi2(name, surName = "friend"):
+    print("Hello " + name + " " + surName)
+    
+    
+sayHi()
+sayHi("Merve")
+sayHi2("Arzu", "Köncü")
+sayHi2("Arzu")
